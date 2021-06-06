d32271d9f19fea053842a53b402f7441b5135f44
Kiran Shila <me@kiranshila.com>
2021-06-06 14:47:11 -0400

    Fixed missing dependencies

--- src/deps.cljs
+++ src/deps.cljs
@@ -3,5 +3,7 @@
             "remark-gfm" "1.0.0"
             "remark-math" "4.0.0"
             "remark-parse" "9.0.0"
+            "remark-footnotes" "3.0.0"
             "unified" "9.2.1"
+            "html-entities" "2.3.2"
             "yaml" "1.10.2"}}
