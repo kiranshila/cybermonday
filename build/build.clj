fa027e3d8d2efdafc8a54d3c6721710871634733
Kiran Shila <me@kiranshila.com>
2021-08-28 10:26:01 -0700

    Use the sha, not the tag for the scm tag

--- build/build.clj
+++ build/build.clj
@@ -1,9 +1,20 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+            [org.corfield.build :as bb]
+            [clojure.string :as str]))
 
 (def lib 'com.kiranshila/cybermonday)
 (def version (format "0.2.%s" (b/git-count-revs nil)))
+
+(defn sha
+  [{:keys [dir path] :or {dir "."} :as params}]
+  (-> {:command-args (cond-> ["git" "rev-parse" "HEAD"]
+                       path (conj "--" path))
+       :dir (.getPath (b/resolve-path dir))
+       :out :capture}
+      b/process
+      :out
+      str/trim))
 
 (defn thinjar
   "Create thinjar"
@@ -11,6 +22,7 @@
   (-> opts
       (assoc :lib lib
              :version version
+             :tag (sha nil)
              :scm-url "https://github.com/kiranshila/cybermonday")
       (bb/clean)
       (bb/jar)))
