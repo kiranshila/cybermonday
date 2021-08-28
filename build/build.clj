2e91863707282588ff8601db6a8415b2f743ce44
Kiran Shila <me@kiranshila.com>
2021-08-28 09:04:52 -0700

    Automating builds and deploys

---
+++ build/build.clj
@@ -0,0 +1,21 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
+
+(def lib 'com.kiranshila/cybermonday)
+(def version (format "0.2.%s" (b/git-count-revs nil)))
+
+(defn thinjar
+  "Create thinjar"
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/clean)
+      (bb/jar)))
+
+(defn deploy
+  "Send to Clojars"
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
